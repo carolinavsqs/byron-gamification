8163994eca2b29ae72ae586ab2614ffa4e196064
JotaVe <jotavexd@gmail.com>
2017-10-05 13:07:30 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'mysql_connect.php';
+
+if (!empty($_POST['usrname']) or !empty($_POST['psw'])){
+         $input_usuario    = $_POST['usrname'];
+		 $input_password   = $_POST['psw'];
+}
+else
+{
+    echo"Os campos devem ser preenchidos";
+    exit;
+}   
+
+$input_usuario     		= stripslashes($input_usuario);
+$input_password         = stripslashes($input_password);
+$input_usuario   		= mysqli_real_escape_string($conn,$input_usuario);
+$input_password       	= mysqli_real_escape_string($conn,$input_password);
+
+
+ $queryText = "SELECT * FROM `usuario` WHERE `user` LIKE '".$input_usuario."' AND `password` LIKE '".$input_password."'";
+    $queryResult = mysqli_query ($conn, $queryText);
+
+    if ((!$queryResult) or ((mysqli_num_rows ($queryResult)) < 1))
+    {
+        echo "<script>alert ('Invalid email or password!');
+            window.location.href='../index.php'</script>";
+        exit;
+    }
+    else
+    {
+        session_start ();
+		echo "<script>alert ('Bem Vindo');
+           </script>";
+    }
+
+?>
+	
