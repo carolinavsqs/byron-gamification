e33f6d1137ab05a68840cdc1fa841318da1b4579
JotaVe <jotavexd@gmail.com>
2017-10-05 14:39:43 -0300

    FIX
    Arumando diretorios

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 					<p class="header_login_text">"- Alto lá, viajante! - disse a guarda, em tom intimidador. - Identifique-se!"</p>
 				</div>
 	
-					<form class="form_login" action="login.php" method="POST">
+					<form class="form_login" action="./PHP/login.php" method="POST">
 						<input class="form_login_input form_login_input_name" type="text" placeholder="Nome de Usuário" name="usrname" required>
 						<input class="form_login_input" type="password" placeholder="Senha" name="psw" required>
 						<button class="login_button_internal login_button_internal_submit" type="submit">PASSAR PELOS PORTÕES</button>
