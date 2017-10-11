41084c903e2817569a39f0e9c736cdb7d8551f31
JotaVe <jotavexd@gmail.com>
2017-10-11 15:38:16 -0300

    FIX
    Arrumando o new_user para as coisas que decidimos na reunião

--- _controller/new_user.php
+++ _controller/new_user.php
@@ -2,23 +2,30 @@
     require_once 'mysql_connect.php';
     
 
-    if (!isset($_POST['usrname']) or !isset($_POST['nome']) or !isset($_POST['psw1']) or !isset($_POST['psw2']))
+    if (!isset($_POST['usrname']) or !isset($_POST['nome']) or !isset($_POST['class']) or !isset($_POST['data'])){
+        $input_data = $_POST['data'];
         die ();
+    }   
     
+    $remove     = $_POST['data'];
+    $trimmed    = str_replace('-', '', $remove);
 
     $username   = stripslashes($_POST['usrname']);
-    $nome      = stripslashes($_POST['nome']);
-    $password1  = stripslashes($_POST['psw1']);
-    $password2  = stripslashes($_POST['psw2']);
+    $nome       = stripslashes($_POST['nome']);
+    $class      = stripslashes($_POST['class']);
+    $password1  = stripslashes($trimmed);
+    $data       = stripslashes($_POST['data']);
     $username   = mysqli_real_escape_string($conn, $username);
-    $nome      = mysqli_real_escape_string($conn, $nome);
-    $password1  = mysqli_real_escape_string($conn, $password1);
-    $password2  = mysqli_real_escape_string($conn, $password2);
+    $nome       = mysqli_real_escape_string($conn, $nome);
+    $class      = mysqli_real_escape_string($conn, $class);
+    $password1  = mysqli_real_escape_string($conn, $trimmed);
+    $data       = mysqli_real_escape_string($conn, $data);
+
 
     $password   = hash ("sha256", $password1);
 
-    $queryText  = "INSERT INTO `usuario`(`user`, `name`, `password`) VALUES
-        ('".$username."','".$nome."','".$password."')";
+    $queryText  = "INSERT INTO `usuario`(`user`, `name`, `class`,`dateBirthday`,`password`) VALUES
+        ('".$username."','".$nome."','".$class."', '".$data."','".$password."')";
 
     $queryResult = mysqli_query ($conn, $queryText);
 
