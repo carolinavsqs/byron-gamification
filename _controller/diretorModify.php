c4c2ccd00fca644af7edb9a7b14e44d9bea154a3
carolvasques <carol.vasques4@gmail.com>
2017-10-30 22:06:54 -0200

    Jotave pediu pra dar commit

--- _controller/diretorModify.php
+++ _controller/diretorModify.php
@@ -1,9 +1,10 @@
 <?php
 
+session_start();
 require_once 'mysql_connect.php';
 include "log.php";
 
-$sessionID = //Informação do front end
+$sessionID = $_POST['usrname'];
 
 $query_TEXT = "SELECT * FROM `usuario` WHERE '".$sessionID."' = `user`";
 $query_result = mysqli_query($conn, $query_TEXT);
@@ -11,21 +12,21 @@ $query_result = mysqli_query($conn, $query_TEXT);
 $linha = mysqli_fetch_array($query_result);
 
 
-if (empty($_POST['new_exp']))
+if(!empty($_POST['new_exp']))
 {
     $new_exp       = $_POST['new_exp'];
     $new_exp       = stripslashes ($new_exp);
-    $new_exp       = mysql_real_escape_string ($new_exp);
+    $new_exp       = mysqli_real_escape_string($conn,$new_exp);
     
     $exp_total     = $new_exp."+".$linha['exp'];
-    
+	
     $message = "XP Alterada em + .$new_exp.";
     $type = "xp";
     $user = $_SESSION['byron_gamification']['user'];
     saveLog($message, $type, $user);
 }
 
-    $query_name = "UPDATE `usuario` SET `exp` = '".$new_total."',  WHERE  user = '".$my_user."'";
+    $query_name = "UPDATE `usuario` SET `exp`='".$exp_total."',  WHERE  user = '".$sessionID."'";
 
     $query_result = mysqli_query($conn, $query_name);
 
--- _controller/log.php
+++ _controller/log.php
@@ -6,11 +6,11 @@ function saveLog($mensagem, $type, $user) {
     $hora = date('Y-m-d H:i:s'); // Salva a data e hora atual (formato MySQL)
 // Usamos o mysql_escape_string() para poder inserir a mensagem no banco
 //   sem ter problemas com aspas e outros caracteres
-    $mensagem = mysql_escape_string($mensagem);
+    $mensagem = mysqli_real_escape_string($conn,$mensagem);
 // Monta a query para inserir o log no sistema
     $queryText = "INSERT INTO `log` VALUES(null,'".$hora."','".$ip."','".$mensagem."','".$type."','".$user."')";
     
-    $conn = mysqli_connect("localhost", "root", "", "gamification_db") or die ();
+
     $queryResult = mysqli_query($conn, $queryText);
     if (!$queryResult){
         echo "<script> alert('deu erro filhão!');
