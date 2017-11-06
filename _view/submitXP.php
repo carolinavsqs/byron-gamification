1ae01273baabb3ccb0460fcf22415c7d64e36def
JotaVe <jotavexd@gmail.com>
2017-11-06 21:34:31 -0200

    [FIX]³
    Desculpa gente. Tem q ser o Nick mesmo em vez dos nomes. Ps.: Diretores vao ter q decorar os nicks dos membros

--- _view/submitXP.php
+++ _view/submitXP.php
@@ -48,10 +48,10 @@
 
 											$classe = $_SESSION['byron_gamification']['class'];
 
-											$sql = "SELECT  `name` FROM `usuario` WHERE `class` = '".$classe."'";
+											$sql = "SELECT  `user` FROM `usuario` WHERE `class` = '".$classe."'";
 											$result = mysqli_query($conn,$sql);
 											while($row = mysqli_fetch_array($result)){
-                               					echo "<option>".$row['name']."</option>";
+                               					echo "<option>".$row['user']."</option>";
                                				}
                                				
 
